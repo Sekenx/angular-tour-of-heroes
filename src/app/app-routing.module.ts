03acb0168305a9b504a5bc3d08b16e72be96b0ce
Wasdom Kung <seken@gmx.com>
2017-09-07 14:19:05 +0100

    Fixed missing Routes dependancy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
